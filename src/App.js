7fc81d47c3d05c7a3c692fa8e58277cb4c05d144
sona306 <sonasabu307@gmail.com>
2024-06-13 01:20:09 +0530

    navbar comp

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Add from './components/Add';
 import Search from './components/Search';
 import View from './components/View';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Add/>
-      <Search/>
-      <View/>
-    </div>
+<BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Add/>}/>
+      <Route path='/search' element={<Search/>}/>
+      <Route path='/view' element={<View/>}/>
+    </Routes>
+</BrowserRouter>
   );
 }
 
--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import Nav from './Nav'
 
 const Add = () => {
 
